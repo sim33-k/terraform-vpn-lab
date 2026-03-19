408f59a590017d2eb60c2f642d2ea1f6c5d94cb0
SW33TSTUFF <simaakniyaz@gmail.com>
2026-03-20 02:01:52 +0530

    Add initial Terraform configuration files and .gitignore

---
+++ variables.tf
@@ -0,0 +1,34 @@
+variable "aws_region" {
+  description = "AWS region to deploy into"
+  type        = string
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+}
+
+variable "public_subnet_cidr" {
+  description = "CIDR block for the public subnet"
+  type        = string
+}
+
+variable "private_subnet_cidr" {
+  description = "CIDR block for the private subnet"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type for all instances"
+  type        = string
+}
+
+variable "key_pair_name" {
+  description = "Name of the existing AWS key pair for SSH access"
+  type        = string
+}
+
+variable "your_ip" {
+  description = "Your local machine IP in CIDR notation e.g. 203.0.113.5/32"
+  type        = string
+}
